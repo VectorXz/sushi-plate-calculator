d4b0940fd39ed2855521d2c3a6554ce5e5521975
Patiphol Pussawong <patiphol.pussawong@ibm.com>
2023-03-09 20:38:46 +0700

    feat: add reset button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Sushi-Calculator';
 
   totalPrice = 0;
   totalPlate = 0;
-  currency = "THB";
+  currency = 'THB';
 
   handleAddPlate(price: number): void {
     this.totalPrice += price;
     this.totalPlate += 1;
+  }
+
+  resetCounter(data: boolean) {
+    if (data) {
+      this.totalPrice = 0;
+      this.totalPlate = 0;
+    }
   }
 }
--- src/app/components/total-price-section/total-price-section.component.ts
+++ src/app/components/total-price-section/total-price-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-total-price-section',
@@ -9,4 +9,9 @@ export class TotalPriceSectionComponent {
   @Input() totalPrice: number = 0;
   @Input() totalPlate: number = 0;
   @Input() currency: string = "THB";
+  @Output() onReset = new EventEmitter();
+
+  handleResetCounter(): void {
+    this.onReset.emit(true);
+  }
 }
